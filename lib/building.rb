dd9abce9dcff30e08fd151917b6b34b2eeefca19
Joey Haas <josephthaas@gmail.com>
2021-02-17 10:47:57 -0700

    completed iteration 3

--- lib/building.rb
+++ lib/building.rb
@@ -3,6 +3,7 @@ class Building
   def initialize
     @units =[]
     @renters = []
+    @rented_units = []
   end
 
   def add_unit(unit)
@@ -21,12 +22,72 @@ class Building
     @renters
   end
 
-
   def average_rent
     total_units = @units.count
     total_rent = @units.sum do |unit|
       unit.monthly_rent
     end
     average_rent = total_rent.to_f / total_units.to_f
+  end
+
+  def rented_units
+    @rented_units = []
+
+    not_vacant = @units.find_all do |unit|
+    unit.renter != nil
+    end
+
+    not_vacant.each do |unit|
+      @rented_units << unit
+    end
+    @rented_units
+  end
+
+  def renter_with_highest_rent
+    rents = []
+    @rented_units.each do |units|
+      rents << units.monthly_rent
+    end
+    rents
+    highest_rent = rents.max
+
+    apartment_with_highest_rent = @rented_units.find do |unit|
+      unit.monthly_rent == highest_rent
+    end
+
+    apartment_with_highest_rent.renter
+  end
+
+  def units_by_number_of_bedrooms
+    units_by_number_of_bedrooms = {
+                                  3 => [],
+                                  2 => [],
+                                  1 => []
+                                  }
+
+      units_with_3_bedrooms = @units.find_all do |unit|
+        unit.bedrooms == 3
+      end
+
+      units_with_2_bedrooms = @units.find_all do |unit|
+        unit.bedrooms == 2
+      end
+
+      units_with_1_bedrooms = @units.find_all do |unit|
+        unit.bedrooms == 1
+      end
+
+      units_with_3_bedrooms.each do |unit|
+        units_by_number_of_bedrooms[3] << unit.number
+      end
+
+      units_with_2_bedrooms.each do |unit|
+         units_by_number_of_bedrooms[2] << unit.number
+       end
+
+       units_with_1_bedrooms.each do |unit|
+         units_by_number_of_bedrooms[1] << unit.number
+       end
+       units_by_number_of_bedrooms
   end
 end
--- test/building_test.rb
+++ test/building_test.rb
@@ -45,12 +45,11 @@ class BuildingTest < Minitest::Test
     building.add_unit(unit1)
     building.add_unit(unit2)
     unit1.add_renter(renter1)
-#require 'pry'; binding.pry
+    # require 'pry'; binding.pry
     assert_equal [renter1], building.renters
   end
 
   def test_average_rent
-
     building = Building.new
     unit1 = Apartment.new({ number: 'A1', monthly_rent: 1200, bathrooms: 1, bedrooms: 1 })
     unit2 = Apartment.new({ number: 'B2', monthly_rent: 999, bathrooms: 2, bedrooms: 2 })
@@ -60,7 +59,80 @@ class BuildingTest < Minitest::Test
     building.add_unit(unit2)
     unit1.add_renter(renter1)
     unit2.add_renter(renter2)
-    #require 'pry'; binding.pry
+    # require 'pry'; binding.pry
     assert_equal 1099.5, building.average_rent
+  end
+
+  def test_rented_units
+    building = Building.new
+    unit1 = Apartment.new({ number: 'A1', monthly_rent: 1200, bathrooms: 1, bedrooms: 1 })
+    unit2 = Apartment.new({ number: 'B2', monthly_rent: 999, bathrooms: 1, bedrooms: 2 })
+    unit3 = Apartment.new({ number: 'C3', monthly_rent: 1150, bathrooms: 2, bedrooms: 2 })
+    unit4 = Apartment.new({ number: 'D4', monthly_rent: 1500, bathrooms: 2, bedrooms: 3 })
+    renter1 = Renter.new('Spencer')
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+
+    assert_equal [], building.rented_units
+  end
+
+  def test_can_have_rented_units
+    building = Building.new
+    unit1 = Apartment.new({ number: 'A1', monthly_rent: 1200, bathrooms: 1, bedrooms: 1 })
+    unit2 = Apartment.new({ number: 'B2', monthly_rent: 999, bathrooms: 1, bedrooms: 2 })
+    unit3 = Apartment.new({ number: 'C3', monthly_rent: 1150, bathrooms: 2, bedrooms: 2 })
+    unit4 = Apartment.new({ number: 'D4', monthly_rent: 1500, bathrooms: 2, bedrooms: 3 })
+    renter1 = Renter.new('Spencer')
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+    unit2.add_renter(renter1)
+
+    assert_equal [unit2], building.rented_units
+  end
+
+  def test_renter_with_highest_rent
+    building = Building.new
+    unit1 = Apartment.new({ number: 'A1', monthly_rent: 1200, bathrooms: 1, bedrooms: 1 })
+    unit2 = Apartment.new({ number: 'B2', monthly_rent: 999, bathrooms: 1, bedrooms: 2 })
+    unit3 = Apartment.new({ number: 'C3', monthly_rent: 1150, bathrooms: 2, bedrooms: 2 })
+    unit4 = Apartment.new({ number: 'D4', monthly_rent: 1500, bathrooms: 2, bedrooms: 3 })
+    renter1 = Renter.new('Spencer')
+    renter2 = Renter.new('Jessie')
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+    unit2.add_renter(renter1)
+    unit1.add_renter(renter2)
+    building.rented_units
+    building.renters
+    #require 'pry'; binding.pry
+    assert_equal renter2, building.renter_with_highest_rent
+  end
+
+  def test_units_by_number_of_bedrooms
+    building = Building.new
+    unit1 = Apartment.new({ number: 'A1', monthly_rent: 1200, bathrooms: 1, bedrooms: 1 })
+    unit2 = Apartment.new({ number: 'B2', monthly_rent: 999, bathrooms: 1, bedrooms: 2 })
+    unit3 = Apartment.new({ number: 'C3', monthly_rent: 1150, bathrooms: 2, bedrooms: 2 })
+    unit4 = Apartment.new({ number: 'D4', monthly_rent: 1500, bathrooms: 2, bedrooms: 3 })
+    renter1 = Renter.new('Spencer')
+    renter2 = Renter.new('Jessie')
+    renter3 = Renter.new("Max")
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+    building.add_unit(unit4)
+    unit2.add_renter(renter1)
+    unit1.add_renter(renter2)
+    unit3.add_renter(renter3)
+    building.rented_units
+    building.renters
+
+    expected = {3 =>["D4"], 2 => ["B2", "C3"], 1 => ["A1"]}
+#require 'pry'; binding.pry
+    assert_equal expected, building.units_by_number_of_bedrooms
+
   end
 end
