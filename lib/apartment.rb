fc17b1a54d62a2f2e84a574c2ecf5593a852ba89
Joey Haas <josephthaas@gmail.com>
2021-02-17 09:17:55 -0700

    completed iteration 1

---
+++ lib/apartment.rb
@@ -0,0 +1,18 @@
+class Apartment
+  attr_reader :number,
+              :monthly_rent,
+              :bathrooms,
+              :bedrooms,
+              :renter
+  def initialize(number:, monthly_rent:, bathrooms:, bedrooms:)
+    @number = number
+    @monthly_rent = monthly_rent
+    @bathrooms = bathrooms
+    @bedrooms = bedrooms
+    @renter = ()
+  end
+
+  def add_renter(renter)
+    @renter = renter
+  end
+end
